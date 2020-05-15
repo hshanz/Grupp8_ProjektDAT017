9193578e232eccde80989010e09470c762ae98cf
FHanberg <filhanb@student.chalmers.se>
2020-05-15 08:58:45 +0200

    Designed (temp) Cart Items

--- src/Project/CartItemController.java
+++ src/Project/CartItemController.java
@@ -1,4 +1,25 @@
 package Project;
 
-public class CartItem {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CartItemController implements Initializable{
+
+    @FXML private ImageView CartItemImage;
+
+    @FXML private Label CartItemName;
+
+    @FXML private Label CartItemPrice;
+
+    @FXML private Label CartItemCount;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
