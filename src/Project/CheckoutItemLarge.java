505ea16a33d541ef9e794802a67b6f40fdb043d0
Emil <35796281+ThePussyCat@users.noreply.github.com>
2020-05-20 15:09:06 +0200

    t

--- src/Project/CheckoutItemLarge.java
+++ src/Project/CheckoutItemLarge.java
@@ -1,4 +1,14 @@
 package Project;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class CheckoutItemLarge {
+    @FXML Text name_text; //Should show the name of the wares (ex "Köttfärslimpa")
+    @FXML Text cost_text; //Should show the cost of these wares (ex "120kr")
+    @FXML TextField number_of_wares; //Should show the number of wares (ex "3st"), This is can be edited
+
+    public void AddWare () {} //Should add 1 ware from the cart
+    public void RemoveWare () {} //Should remove 1 ware from the cart
 }
---
+++ src/Project/CheckoutItemSmall.java
@@ -0,0 +1,12 @@
+package Project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class CheckoutItemSmall {
+
+    @FXML Text name_text; //Should show the name of the wares
+    @FXML Text cost_text; //Should show the cost of the wares
+    @FXML TextField number_of_wares; //should show the number of these wares in the cart. This can also be edited
+}
