87b86f01648e6a9e346bfa0c8d829d6bf7ec864e
Emil <35796281+ThePussyCat@users.noreply.github.com>
2020-05-20 16:04:24 +0200

    t

--- src/Project/CheckoutItemSmall.java
+++ src/Project/CheckoutItemSmall.java
@@ -5,6 +5,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class CheckoutItemSmall {
+    /* The checkout item small should populate the fourth step of the checkout wizard.
+     * It should be checkout item large for each type of ware in the cart.
+     * The checkout item small should be placed in a scroll pane where 2 items is besides each other and then it lists down. '
+     * Reference image: https://www.figma.com/file/Y6tseIZ6XOMK9EmOdySxMK/IMat?node-id=0%3A1 Betalningswizard4*/
 
     @FXML Text name_text; //Should show the name of the wares
     @FXML Text cost_text; //Should show the cost of the wares
---
+++ src/Project/CheckoutTime.java
@@ -0,0 +1,12 @@
+package Project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CheckoutTime {
+    /* Should populate the choose time popup in the checkout.
+     * Reference image: https://www.figma.com/file/Y6tseIZ6XOMK9EmOdySxMK/IMat?node-id=0%3A1 Betalningswizard2chosing */
+    @FXML Button time_button; //Should display a set time span of ca 30 min (ex "16:00-16:30")
+
+    public void ChooseTime() {} //Choose this specific time when the wares should be  delivered
+}
